205978f4fd249a130d2caf2f97a3a6048c90376a
Newshinsky <72308491+Newshinsky@users.noreply.github.com>
2021-02-20 16:00:46 +0300

    fix: add a check for an empty array

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(array) {
+if (array.length == 0){
+return 0
+}
 let cats = 0;
 let concatenated = array.reduce( function (previous, current) {
         return previous.concat(current);
