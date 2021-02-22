2bc0d8e648bed336b73f1ff385b4daf1221a52ed
Newshinsky <72308491+Newshinsky@users.noreply.github.com>
2021-02-22 22:50:27 +0300

    feat: implement dream team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(array) {
+
+  let checkArray;
+  if (checkArray = Array.isArray(array)) {
+
+    array = array;
+  } else {
+    return false;
+  }
+  if (arguments.length == 0) {
+    return null;
+  }
+
+
+
+  const result = array.reduce((accumulator, currentValue) => {
+    const item = currentValue.trim()
+
+    if (item !== '') {
+      accumulator.push(item);
+    }
+
+    return accumulator;
+  }, [])
+
+  let firstUpperLetter = result.map(upPer);
+  function upPer(value) {
+    return value[0].toUpperCase() + value.substr(1);
+  }
+  let firstLetter = firstUpperLetter.sort().map(e => e[0])
+
+  return firstLetter.join('');
 };
