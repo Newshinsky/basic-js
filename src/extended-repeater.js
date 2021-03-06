262152a5718a3642a233f72c489f7c6adbf90861
Newshinsky <72308491+Newshinsky@users.noreply.github.com>
2021-03-06 18:22:31 +0300

    feat: implement repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,62 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    str = String(str)
+
+  let repeatTimes = options.repeatTimes;
+  if (repeatTimes === undefined) {
+  repeatTimes = 1;
+  }
+  
+  let separator  = options.separator;
+  if (separator === undefined){
+  separator = '+'
+  }
+  
+  let addition = options.addition;
+  if (addition === undefined){
+  addition = ''
+  } else {
+  addition = String(addition);
+  }
+  
+  let additionRepeatTimes  = options.additionRepeatTimes;
+  if (additionRepeatTimes === undefined){
+  additionRepeatTimes = 1;
+  }
+  
+  let additionSeparator  = options.additionSeparator;
+  if (additionSeparator === undefined){
+  additionSeparator = '|'
+  }
+  
+    
+    let newStr = '';
+    let addStr = '';
+    let result = '';
+    
+    
+    for (j = 0; j < additionRepeatTimes; j++ ){
+    
+    
+   addStr += addition + additionSeparator;
+  
+   } 
+   
+   addStr =  addStr.slice(0,addStr.length - additionSeparator.length)
+    
+   newStr = str + addStr 
+   
+   for (i = 0; i < repeatTimes; i++ ){
+    
+  
+  result += newStr + separator; 
+   
+   }
+   
+  result =  result.slice(0,result.length - separator.length)
+  
+  return result;
+  
 };
   
