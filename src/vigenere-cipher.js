bf2be56875cf9ed8d0d169c8d18b02c1b3b06751
Newshinsky <72308491+Newshinsky@users.noreply.github.com>
2021-03-29 20:43:41 +0300

    fix: fix error

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,172 +1,148 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  direct = true
-  constructor(type) {
-    if (type === false) {
-      this.direct = false
+
+  constructor(direct) {
+    this.direct = true;
+    if (direct === false) {
+      this.direct = false;
     }
   }
-  encrypt(message, key ){
-    if (!message || !key) throw "Error";
-    /****************Приводим в порядок аргументы***********************/
-    
-    let newMessage = message.toLowerCase().replace(/\s+/g, '').trim();
-    let newKey = key.toLowerCase().replace(/\s+/g, '').trim()
+
+
+  encrypt(message, key) {
+    if (arguments.length < 2) throw new Error;
+
+    let strArr = message.toLowerCase().split('');
+    let lettersArr = strArr.filter(val => val.match(/[a-z]/));
+    let newMessage = lettersArr.join('');
+    let newKey = key.toLowerCase();
   
     /**************** Проверяем Длину***********************/
     
     for (let i = 0; i < newMessage.length; i++) {  
         newKey += newKey[i%newKey.length];
       }
-      newKey = newKey.slice(0,newMessage.length)
+      newKey = newKey.slice(0,newMessage.length);
   
     /****************Получаем цифры сообщения и ключа***********************/
     
     
-    newMessage = newMessage.split('')
-    newKey = newKey.split('')
+    newMessage = newMessage.split('');
+    newKey = newKey.split('');
     
-    let arrMes = newMessage.map(letter => letter.charCodeAt(0) - 97)
-    let arrkey = newKey.map(letter => letter.charCodeAt(0) - 97)
-    
+    let arrMes = newMessage.map(letter => letter.charCodeAt(0) - 97);
+    let arrkey = newKey.map(letter => letter.charCodeAt(0) - 97);
 
     
     
     /****************Суммируем значения***********************/
     
-    let arrNum = []
+    let arrNum = [];
     
     for (i = 0; i<arrMes.length; i++){
     
-    let sum = arrMes[i] + arrkey[i]
+    let sum = arrMes[i] + arrkey[i];
     if (sum >= 26){
     
-    sum =  sum - 26 
+    sum =  sum - 26 ;
     }
-    arrNum.push(sum)
+    arrNum.push(sum);
     
     }
 
     /****************Получаем слово***********************/
     
-    let wordNum = arrNum.map(function(num){
-    return String.fromCharCode(num+97)
-    })
-    
-    wordNum = wordNum.join("").toUpperCase();
-    
-    
-    
-    
-    let final = ''
-    for(j = 0; j <message.length; j++){
-    if(/^[a-zA-Z]+$/.test(message[j])){
-    
-    final += wordNum[j]
-    
-    } else {
-    
-    
-    
-    final += message[j]
-    wordNum = wordNum.slice(0,j) + " " + wordNum.slice(j)
+    let cipher = [];
+    for (let h = 0; h < arrNum.length; h++) {
+      cipher.push(String.fromCharCode(arrNum[h] + 97));
     }
-    
-    }
-    if (this.direct){
 
-       return final;
-      
+    let final = [];
+    let count = 0;
+    for (let r = 0; r < strArr.length; r++) {
+      if (!strArr[r].match(/[a-z]/)) {
+        final.push(strArr[r]);
+        count++;
       } else {
-
-    return final.split('').reverse().join('');
-
+        final.push(cipher[r - count]);
+      }
     }
+
+    if (!this.direct) final.reverse();
+    return final.join('').toUpperCase();
   }
+
+
+  decrypt(message, key) {
+    if (arguments.length < 2) throw new Error;
+
+    if (!message || !key) throw "Error";
+    /****************Приводим в порядок аргументы***********************/
+    let strArr = message.toLowerCase().split('');
+    let lettersArr = strArr.filter(val => val.match(/[a-z]/));
+    let newMessage = lettersArr.join('');
+    let newKey = key.toLowerCase();
+
     
-    decrypt(message, key ){
-
-      if (!message || !key) throw "Error";
-      /****************Приводим в порядок аргументы***********************/
-      let newMessage = message.toLowerCase().replace(/\s+/g, '').trim();
-      let newKey = key.toLowerCase().replace(/\s+/g, '').trim()
-
-      
-      
-      
-      /******************** Проверяем Длину*************************/
-      
-      for (let i = 0; i < newMessage.length; i++) {  
-          newKey += newKey[i%newKey.length];
-        }
-        newKey = newKey.slice(0,newMessage.length)
-  
-      
-      
-      /****************Получаем цифры сообщения и ключа***********************/
-      
-      
-      newMessage = newMessage.split('')
-      newKey = newKey.split('')
-      
-      let arrMes = newMessage.map(letter => letter.charCodeAt(0) - 97)
-      let arrkey = newKey.map(letter => letter.charCodeAt(0) - 97)
-
-      
-      /****************Суммируем значения***********************/
-      
-      let arrNum = []
-      
-      for (i = 0; i<arrMes.length; i++){
-      
-      let sum = arrMes[i] - arrkey[i]
-      if (sum < 0){
-      
-      sum =  arrMes[i] - arrkey[i] + 26
+    
+    
+    /******************** Проверяем Длину*************************/
+    
+    for (let i = 0; i < newMessage.length; i++) {  
+        newKey += newKey[i%newKey.length];
       }
-      arrNum.push(sum)
-      
-      }
+      newKey = newKey.slice(0,newMessage.length);
 
-      
-      /****************Получаем слово***********************/
-      
-      let wordNum = arrNum.map(function(num){
-      return String.fromCharCode(num+97)
-      })
-      
-      wordNum = wordNum.join("").toUpperCase();
-      
-      
-      
-      
-      let final = ''
-      for(j = 0; j <message.length; j++){
-      if(/^[a-zA-Z]+$/.test(message[j])){
-      
-      final += wordNum[j]
-      
+    
+    
+    /****************Получаем цифры сообщения и ключа***********************/
+    
+    
+    newMessage = newMessage.split('');
+    newKey = newKey.split('');
+    
+    let arrMes = newMessage.map(letter => letter.charCodeAt(0) - 97);
+    let arrkey = newKey.map(letter => letter.charCodeAt(0) - 97);
+
+    
+    /****************Суммируем значения***********************/
+    
+    let arrNum = [];
+    
+    for (i = 0; i<arrMes.length; i++){
+    
+    let sum = arrMes[i] - arrkey[i];
+    if (sum < 0){
+    
+    sum =  arrMes[i] - arrkey[i] + 26;
+    }
+    arrNum.push(sum);
+    
+    }
+
+    
+    /****************Получаем слово***********************/
+    let string = [];
+    for (let h = 0; h < arrNum.length; h++) {
+      string.push(String.fromCharCode(arrNum[h] + 97));
+    }
+
+    let final = [];
+    let count = 0;
+    for (let r = 0; r < strArr.length; r++) {
+      if (!strArr[r].match(/[a-z]/)) {
+        final.push(strArr[r]);
+        count++;
       } else {
-      
-      
-      
-      final += message[j]
-      wordNum = wordNum.slice(0,j) + " " + wordNum.slice(j)
+        final.push(string[r - count]);
       }
-      
-      }
-      if (this.direct){
+    }
 
-        return final;
-       
-       } else {
- 
-     return final.split('').reverse().join('');
- 
-     }
-      }
-      
+    if (!this.direct) final.reverse();
+    return final.join('').toUpperCase();
+  }
+
 }
 
 module.exports = VigenereCipheringMachine;
