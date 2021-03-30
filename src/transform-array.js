fa75cb6b076036fa2f1ae43bfb97c77ed6a8585f
Newshinsky <72308491+Newshinsky@users.noreply.github.com>
2021-03-30 10:53:15 +0300

    fix: fix error

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,54 +1,27 @@
 const CustomError = require("../extensions/custom-error");
-
 module.exports = function transform(arr) {
-if (!Array.isArray(arr)) throw 'Error'
-
-
-let options = [
-'--discard-next',
-'--double-next',
-'--discard-prev',
-'--double-prev' 
-]
-
-
-let newArr = [...arr];
-
-for (i = 0; i < newArr.length; i++){
-
-if (newArr.includes(options[0])) {
-
-newArr.splice(newArr.indexOf(options[0]) + 1, 1, null)
-
-} 
-
-if (newArr.includes(options[1])) {
-
-newArr.splice(newArr.indexOf(options[1]) , 1, newArr[newArr.indexOf(options[1])+1])
-
-}
-
-if (newArr.includes(options[2])) {
-if(newArr.indexOf(options[2]) === 0) {
-newArr.shift()
-
-} else {
-newArr.splice(newArr.indexOf(options[2]) -1 , 1, null)
-}
-}
-
-if (newArr.includes(options[3])) {
-
-newArr.splice(newArr.indexOf(options[3]) , 1, newArr[newArr.indexOf(options[3])-1])
-
-}
-}
-
-newArr = newArr.filter(function( element ) {
-
-   return element !== null && element !== '--discard-prev' && element !== '--discard-next' && element !== undefined;
-   
-});
-
-return newArr;
+    if (!Array.isArray(arr)) throw 'Error'
+    let options = ['--discard-next', '--double-next', '--discard-prev', '--double-prev']
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === options[0]) {
+            i++;
+        } else if (arr[i] === options[1]) {
+            newArr.push(arr[i + 1]);
+        } else if (arr[i] === options[2]) {
+            if (arr[i - 2] !== options[0]) {
+                newArr.pop();
+            }
+        } else if (arr[i] === options[3]) {
+            if (arr[i - 2] !== options[0]) {
+                newArr.push(newArr[newArr.length - 1]);
+            }
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    newArr = newArr.filter(function(element) {
+        return element !== undefined;
+    });
+    return newArr;
 };
